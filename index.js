fd15fedab658a50f7957c74d84b2c6245ad50735
Gideon1107 <ayenigideon1107@gmail.com>
2024-04-14 01:53:07 +0100

    Added game functionalities

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
+var gameOn = false;
+var level = 0;
+var userClickedPattern = [];
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-function nextSequence() {
-    var randomNumber = Math.ceil(Math.random()*3);
-    var randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
-    
+$(".btn").on("click", function(event) {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
+$(document).on("keypress", function(event) {
+    if (!gameOn) {
+        nextSequence();
+        gameOn = true;
+        $("#level-title").text("LEVEL: " + level)
+    }
+})
+
+
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
 }
 
 
+function playSound(name) {
+    var audio = new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
 
-
-
-
+function nextSequence() {
+    level++;
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeOut(200).fadeIn(200);
+    playSound(randomChosenColour);
+    $("#level-title").text("LEVEL: " + level)
+    
+}
